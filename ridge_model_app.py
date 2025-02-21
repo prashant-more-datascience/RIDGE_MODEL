93c895a3fc98acc7bda9e8af19c1c47c19893d52
prashant-more-datascience <pm257352@gmail.com>
2025-02-21 11:54:49 +0530

    Update ridge_model_app.py

--- ridge_model_app.py
+++ ridge_model_app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pickle
 import numpy as np
 import requests
-from sklearn.preprocessing import StandardScaler
 import plotly.graph_objects as go
 import plotly.express as px
 
